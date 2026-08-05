95451118bbc23ae7241af1210592b312a3a2c158
Mannat Jain <226103934+mannatjain11465-netizen@users.noreply.github.com>
2026-08-05 08:17:09 +0530

    Complete authentication module with route protection

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import PublicRoute from "./components/PublicRoute";
 import ProtectedRoute from "./components/ProtectedRoute";
-
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
@@ -21,13 +20,24 @@ function App() {
 
             <Routes>
 
-                {/* Public Routes */}
+                <Route
+                    path="/login"
+                    element={
+                        <PublicRoute>
+                            <Login />
+                        </PublicRoute>
+                    }
+                />
+                
 
-                <Route path="/login" element={<Login />} />
-
-                <Route path="/register" element={<Register />} />
-
-                {/* Protected Routes */}
+                <Route
+                    path="/register"
+                    element={
+                        <PublicRoute>
+                            <Register />
+                        </PublicRoute>
+                    }
+                />
 
                 <Route
                     path="/dashboard"
